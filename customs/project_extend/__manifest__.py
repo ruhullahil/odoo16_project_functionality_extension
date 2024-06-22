e4ce5bb9e705a729d9fe522ad8aa6a779f78fdd4
ruhullahil <ruhullahilt@gmail.com>
2024-06-22 12:26:17 +0600

    [ADD] record rule for project team

--- customs/project_extend/__manifest__.py
+++ customs/project_extend/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Project Entend",
+    'name': "Project Extend",
 
     'summary': """
     this module used for extend project functionality some record rule and add teams
@@ -20,11 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','project_team'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'security/security.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
